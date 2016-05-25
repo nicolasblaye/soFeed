1b477d581f919b2aa1ff5cc0719dbc98aa2a7675
nicolasblaye <nicolas_blaye@hotmail.fr>
2016-05-25 15:41:39 +0200

    add logo

--- SoFeed/app/src/main/java/fr/infotel/sofeed/LoginActivity.java
+++ SoFeed/app/src/main/java/fr/infotel/sofeed/LoginActivity.java
@@ -88,7 +88,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         });
 
         mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
     }
 
     private void populateAutoComplete() {
