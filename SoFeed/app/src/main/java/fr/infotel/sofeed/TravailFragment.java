89bfe479e3988c03c48c5bd4319ab0468dfa3b36
nicolasblaye <nicolas_blaye@hotmail.fr>
2016-05-25 14:08:13 +0200

    work interface

--- SoFeed/app/src/main/java/fr/infotel/sofeed/TravailFragment.java
+++ SoFeed/app/src/main/java/fr/infotel/sofeed/TravailFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by nicolas on 22/05/16.
@@ -15,6 +17,8 @@ public class TravailFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.travail,container,false);
+        ListView listView = (ListView) v.findViewById(R.id.members);
+        listView.setAdapter(new ArrayAdapter<String>(getActivity(),R.layout.drawer_list_item, getResources().getStringArray(R.array.members)));
         return v;
     }
 }
