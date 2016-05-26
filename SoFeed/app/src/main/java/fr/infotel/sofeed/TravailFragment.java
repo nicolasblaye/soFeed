27f157c33e17601589c3cc32700d8f35f6b1ea2c
nicolasblaye <nicolas_blaye@hotmail.fr>
2016-05-26 10:46:59 +0200

    stub tickets

--- SoFeed/app/src/main/java/fr/infotel/sofeed/TravailFragment.java
+++ SoFeed/app/src/main/java/fr/infotel/sofeed/TravailFragment.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import fr.infotel.sofeed.bean.Ticket;
+
 /**
  * Created by nicolas on 22/05/16.
  */
@@ -18,7 +20,17 @@ public class TravailFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.travail,container,false);
         ListView listView = (ListView) v.findViewById(R.id.members);
-        listView.setAdapter(new ArrayAdapter<String>(getActivity(),R.layout.drawer_list_item, getResources().getStringArray(R.array.members)));
+        listView.setAdapter(new ArrayAdapter<String>(getActivity(),R.layout.travail_list_item, getResources().getStringArray(R.array.members)));
+        ListView listViewTicket = (ListView) v.findViewById(R.id.tickets);
+        // Il faut prendre les tickets sur Rest, stub ici
+        Ticket ticket = new Ticket();
+        ticket.setName("Développer le Service REST");
+        Ticket ticket2 = new Ticket();
+        ticket2.setName("Set up le projet GitHub");
+        String[]tickets = new String[2];
+        tickets[0] = ticket.getName();
+        tickets[1] = ticket2.getName();
+        listViewTicket.setAdapter(new ArrayAdapter<String>(getActivity(),R.layout.travail_list_item, tickets));
         return v;
     }
 }
